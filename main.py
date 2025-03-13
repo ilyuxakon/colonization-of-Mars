cc5cf0b05a0f0f48fc2c74eed28f65ea4538ab52
ilyuxa.kon <ilyuxa.kon@gmail.com>
2025-03-13 22:11:19 +0300

    Результат отбора

--- main.py
+++ main.py
@@ -179,5 +179,30 @@ def choice(planet_name):
                   </body>
                 </html>'''
 
+
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def result(nickname, level, rating):
+    return f'''<!doctype html>
+              <html lang="en">
+                <head>
+                  <meta charset="utf-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                  <link rel="stylesheet" 
+                  href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                  integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                  crossorigin="anonymous">
+                  <link rel="stylesheet" href="{flask.url_for('static', filename='css/style.css')}">
+                  <title>Привет, Яндекс!</title>
+                </head>
+                <body>
+                  <h1>Результаты отбора</h2>
+                  <h6>Претендент на участие в миссии {nickname}:</h6>
+                  <div class="alert alert-success" role="alert">Поздравляем! Ваш рейтинг после {level} этапа отбора</div>
+                  <h6>составляет {rating}!</h6>
+                  <div class="alert alert-warning" role="alert">Желаем удачи</div>
+                </body>
+              </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
