ba1313becb2b0701472b0862a27868ee7ea9b9c8
ilyuxa.kon <ilyuxa.kon@gmail.com>
2025-03-13 19:04:46 +0300

    Реклама с картинкой

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ def slogan():
 @app.route('/promotion')
 def promotion():
     return '<br>'.join(['Человечество вырастает из детства.',
-                         'Человечеству мала одна планета.',
-                         'Мы сделаем обитаемыми безжизненные пока планеты.',
-                         'И начнем с Марса!',
-                         'Присоединяйся!'])
+                        'Человечеству мала одна планета.',
+                        'Мы сделаем обитаемыми безжизненные пока планеты.',
+                        'И начнем с Марса!',
+                        'Присоединяйся!'])
 
 
 @app.route('/image_mars')
@@ -28,6 +28,42 @@ def hello_mars():
     return f'''<h1>Жди нас, Марс!</h1>
                <img src="{flask.url_for('static', filename='img/mars.png')}">
                <p>Вот она какая, красная планета</p>'''
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" href="{flask.url_for('static', filename='css/style.css')}">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Привет, Яндекс!</h1>
+                    <img src="{flask.url_for('static', filename='img/mars.png')}">
+                    <div class="alert alert-dark" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!
+                    </div>
+                  </body>
+                </html>'''
 
 
 if __name__ == '__main__':
